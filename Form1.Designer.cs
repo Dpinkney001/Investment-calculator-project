f0437e4b246724ed4070000b28fc1d50d6549cef
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2019-07-08 02:39:57 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace Target_Investment_calculator_version_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 44);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(776, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Filename to output data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "enter the target amount you wish to save to invest";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(308, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "enter the number of YEARS in which you wish to reach this goal";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(319, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "enter the number of MONTHS in which you wish to reach this goal";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(773, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ InvestmentCalculator.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Duvall pinkney  7/5/2015 version 1
+// version 2.0 7/19/2015
+//version 3.0 9/14/2015
+// Version 4.0 2/12/2019 C# conversion
+// program to analyize users target goal to save for investment and outputs the amount each  (months, years)
+// needed to reach that target goal.
+
+
+namespace Target_Investment_calculator_version_4
+{
+    static class InvestmentCalculator
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // if user pressed enter key
+            if (e.KeyCode == Keys.Enter)
+            {
+                // create a filename to write to file
+                string fileName = inputTextBox_KeyDown.Text;
+
+                // determine whether fileName is a file
+                if (FileDialog.Exists(fileName))
+                {
+                    // get files creation date, modification date etc
+                    GetInformation(fileName);
+                    StreamReader stream = null; // declare StreamReader
+                    try
+                    {
+                        // obtain reader and file contents
+                        using (stream = new StreamReader(fileName))
+                        {
+                            outputTextBox.AppendText(stream.ReadToEnd());
+                        }// end using
+                    }// end try
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }// end catch
+                }// end if
+                // determine whether fileName is a directory
+                else if (Directory.Exists(fileName))
+                {
+                    // get directory's creations date,
+                    // modification date, etc
+                    GetInformation(fileName);
+
+
+                    //obtain directory list of specified directory
+                    string[] directoryList = Directory.GetDirectories(fileName);
+
+                    outputTextBox.AppendText("Directory contents:\n");
+
+                    // output directoryList contents
+                    foreach (var directory in directoryList)
+                        outputTextBox.AppendText("Directory contents:\n");
+                } // end else if
+                else
+                {
+                    // notify user that neither file nor directory exists
+                    MessageBox.Show(inputTextBox_KeyDown.Text + " does not exist", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }// end else
+            }// end if
+        }// end method inputTextBox_KeyDown
+
+        // get information on file or directory,
+        // and output it to outputTextBox
+        private void GetInformation( string fileName )
+        {
+            outputTextBox.clear();
+
+            outputTextBox.AppendText( fileName + " exist\n" );
+
+            outputTextBox.AppendText("Created: " + FileDialog.GetCreationTime(fileName) + "\n");
+
+
+        }
+                    //
+
+                }
+            }
+        }
+
+/*
+
+# write outputs into a file investmentCalcReport.txt
+#
+#
+        def outputfile():
+    outfile = open("investmentCalcReport.txt", 'w')
+
+
+
+
+# grab input values from the user
+        def targetAmountInput():
+    targetAmount = eval(input("Please enter the target amount you wish to save to invest: "))
+    targetYears = eval(input("Please enter the number of YEARS in which you wish to reach this goal: "))
+    targetMonths = eval(input("Please enter the number of MONTHS in which you wish to reach this goal: "))
+    # target pay periods aka target ever 2 weeks
+    Console.WriteLine("Please enter the target amount you wish to save to invest: ");
+    targetAmount = Console.ReadLine()
+
+    targetAmountPerYear = 0.0
+    targetAmountPerYear = targetAmount / targetYears
+    print("You need to save", targetAmountPerYear," every year to reach your goal")
+
+    months = 0
+    months = targetYears* 12
+    amountPerMonths = targetAmount / months
+    print("or save ", amountPerMonths," for ", months,"month(s).")
+
+    targetAmountPerMonth = 0.0
+    targetAmountPerMonth = targetAmount / 12
+    print("You need to save", targetAmountPerMonth," every month to reach your goal")
+    
+    
+    return targetAmount, targetYears, targetMonths, months
+
+
+
+def Jobs() :
+    numOfJobs = eval(input("How many jobs do you work? Enter that number: "))
+    jobcounter = 0
+    while jobcounter<numOfJobs:
+            
+        for jobs in range(1,numOfJobs + 1):
+            jobs = 1
+            monthlyIncomeTotal = 0
+            
+            print("For job number ", jobs," Do you get paid by the hour or by salary? ")
+            kindOfPay = input("Enter SALARY of HOURLY here: ")
+            if kindOfPay == "HOURLY":
+                # example 17.00 an hour times 40 hours per week
+                print("")
+                hourlyRate = eval(input("How much do you get paid an hour? Enter that amount: "))
+                hoursPerWeek = eval(input("How many hours do you work per week? Enter that amount: "))
+                wagesPerWeek = hourlyRate* hoursPerWeek
+                wagesPerYear = wagesPerWeek* 52
+                # monthly wages
+                monthlyWages = hourlyRate* hoursPerWeek * 4
+                print("")
+                print("your monthly income is $", monthlyWages," before taxes.")
+                print("")
+                print("your yearly income is $", wagesPerYear," before taxes.")
+
+
+                jobs + 1
+                jobcounter + 1
+                monthlyIncomeTotal = monthlyIncomeTotal + monthlyWages
+
+            elif kindOfPay == "hourly":
+                # example 17.00 an hour times 40 hours per week
+                print("")
+                hourlyRate = eval(input("How much do you get paid an hour? Enter that amount: "))
+                hoursPerWeek = eval(input("How many hours do you work per week? Enter that amount: "))
+                wagesPerWeek = hourlyRate* hoursPerWeek
+                wagesPerYear = wagesPerWeek* 52
+                # monthly wages
+                monthlyWages = hourlyRate* hoursPerWeek * 4
+                print("")
+                print("your monthly income is $", monthlyWages," before taxes.")
+                print("")
+                print("your yearly income is $", wagesPerYear," before taxes.")
+
+
+                jobs + 1
+                jobcounter + 1
+                monthlyIncomeTotal = monthlyIncomeTotal + monthlyWages
+
+
+            elif kindOfPay == "SALARY":
+                print("")
+                salary = eval(input("What is your salary? Enter that amount: "))
+                monthlyIncome = salary / 12
+                print("your monthly income is $", monthlyIncome)
+                biweeklyIncome = monthlyIncome / 2
+                print("")
+                print("Your biweekly income is $", biweeklyIncome)
+
+
+                jobs + 1
+                jobcounter + 1
+                monthlyIncomeTotal = monthlyIncomeTotal + monthlyIncome
+
+            elif kindOfPay == "salary":
+                print("")
+                salary = eval(input("What is your salary? Enter that amount: "))
+                monthlyIncome = salary / 12
+                print("your monthly income is $", monthlyIncome)
+                biweeklyIncome = monthlyIncome / 2
+                print("")
+                print("Your biweekly income is $", biweeklyIncome)
+
+
+                jobs + 1
+                jobcounter + 1
+                monthlyIncomeTotal = monthlyIncomeTotal + monthlyIncome
+                
+            else:
+                print("Please enter your type of pay. Hourly or salary: ")
+                break;
+            print("-----------------------------------------------------------")
+            print("Your total monthly income is $", monthlyIncomeTotal)
+        return monthlyIncomeTotal
+
+
+
+
+
+def expenses(monthlyIncomeTotal) :
+    totalMonthlyCostOfLiving = 0
+    lifeexpenses = 0
+    while totalMonthlyCostOfLiving != -1:
+         totalMonthlyCostOfLiving = eval(input("What is the total cost of living for the month? Enter that amount or -1 to exit: "))
+    lifeexpenses = totalMonthlyCostOfLiving + lifeexpenses
+
+    return lifeexpenses
+
+def savings(monthlyIncomeTotal, targetAmount, targetYears, targetMonths) :
+    targetSavings = eval(input("What is the target amount you are trying to save for your life savings? Enter the amount: "))
+    
+    
+    savingPerMonth = eval(input("How much are you planning to save for your life savings each month? Enter that amount: "))
+    
+    lifeSavingsACC = savingPerMonth* targetMonths
+    remainingAmount = targetSavings - lifeSavingsACC
+    print("At this rate of ", savingPerMonth,"for ", targetMonths,"months, you would have saved ", lifeSavingsACC,".")
+    if monthlyIncomeTotal > targetAmount:
+        excessAmount = monthlyIncomeTotal - targetAmount
+        print("The excess amount is: ", excessAmount)
+
+    elif targetAmount > monthlyIncomeTotal:
+        deficitAmount = targetAmount - monthlyIncomeTotal
+        print("The amount remaining is: ", deficitAmount)
+
+    elif targetAmount == monthlyIncomeTotal:
+        print("Your income and  target amount are equal!")
+
+
+
+
+# function calls for main program
+        def main():
+
+    print("This program was written by Duvall Pinkney. This program calculate the amount of time needed to raise target amount of money for investment")
+    print("version 3.0")
+    print("*************************************************************************************************************************************")
+
+    monthlyIncomeTotal = Jobs()
+    targetAmount, targetYears, targetMonths, months = targetAmountInput()
+    savings(monthlyIncomeTotal, targetAmount, targetYears, targetMonths)
+    expenses(monthlyIncomeTotal)
+
+
+
+
+
+
+
+
+    print("Amount needed each month is : ", targetAmountPerMonth)
+    print("Amount needed each years is : ", targetAmountPerYear)
+    #print("Amount needed every 2 weeks is: ",)
+   
+main()
+    }
+}
+*/
